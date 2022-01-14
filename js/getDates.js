4751ce650bb5b63815595ec93808a3d58c9ffd08
ribeka <ribekananjo@gmail.com>
2022-01-14 12:04:38 +0900

    updated

--- js/getDates.js
+++ js/getDates.js
@@ -21,9 +21,27 @@ const months = [
 	"November",
 	"December",
 ];
+
 const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
-const fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+const hours = d.getHours();
+const minutes = d.getMinutes();
+const second = d.getSeconds();
+const fulldate =
+	dayName +
+	", " +
+	monthName +
+	" " +
+	d.getDate() +
+	", " +
+	year +
+	" " +
+	hours +
+	":" +
+	minutes +
+	":" +
+	second +
+	" (JST)";
 document.getElementById("currentdate").textContent = fulldate;
