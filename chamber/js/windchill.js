db08ea222e8e10ae8d4fa7496d770d3e4391810f
ribeka <ribekananjo@gmail.com>
2022-02-16 21:48:21 +0900

    updated

---
+++ chamber/js/windchill.js
@@ -0,0 +1,21 @@
+let t = parseFloat(document.querySelector("#t").textContent);
+let s = parseFloat(document.querySelector("#s").textContent);
+let windchill = "";
+
+if (t <= 50 && s > 3) {
+	windchill = windChill(t, s);
+	windchill = `${windchill}&#8451;`;
+} else {
+	windchill = "N/A";
+}
+
+document.querySelector("#w").innerHTML = windchill;
+
+function windChill(temp, speed) {
+	return Math.round(
+		35.74 +
+			0.6215 * temp -
+			35.75 * Math.pow(speed, 0.16) +
+			0.4275 * temp * Math.pow(speed, 0.16)
+	);
+}
