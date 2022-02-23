6d796279c20f15e7ddc5f334bea159bda04ac6b0
ribeka <ribekananjo@gmail.com>
2022-02-23 15:40:13 +0900

    updated

--- lesson8/tablebuild.js
+++ lesson8/tablebuild.js
@@ -28,3 +28,28 @@ const year = d.getFullYear();
 const fulldate =
 	"Compiled " + monthName + " " + d.getDate() + ", " + year + " by Ribeka";
 document.getElementById("currentdate").textContent = fulldate;
+
+const dayName = daynames[d.getDay()];
+const hours = d.getHours();
+const minutes = d.getMinutes();
+const second = d.getSeconds();
+const fulldate2 =
+	dayName +
+	", " +
+	monthName +
+	" " +
+	d.getDate() +
+	", " +
+	year +
+	" " +
+	hours +
+	":" +
+	minutes +
+	":" +
+	second +
+	" (JST)";
+document.getElementById("currentdate2").textContent = fulldate2;
+
+//year
+var today = new Date();
+document.querySelector("#year").textContent = today.getFullYear();
