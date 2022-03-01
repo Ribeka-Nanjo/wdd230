d7e51ba93b29a065928b4d2ea269e09b51461f80
ribeka <ribekananjo@gmail.com>
2022-03-01 23:47:22 +0900

    updated

--- chamber/js/homedate.js
+++ chamber/js/homedate.js
@@ -35,14 +35,3 @@ if (todaysdate == 1 || todaysdate == 2) {
 	let x = document.querySelector("#text");
 	x.innerHTML = `🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.`;
 }
-
-//Email-address
-const email = document.getElementById("mail");
-
-email.addEventListener("input", function (event) {
-	if (email.validity.typeMismatch) {
-		email.setCustomValidity("provide us your e-mail address!");
-	} else {
-		email.setCustomValidity("");
-	}
-});
