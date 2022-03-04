899c9610bd136c48d428ebb6ac41710b774a7070
ribeka <ribekananjo@gmail.com>
2022-03-04 13:57:49 +0900

    updated

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,5 +1,5 @@
 const requestURL =
-	"https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+	"https://Ribeka-Nanjo.github./wdd230/lesson9/data/example.json";
 
 const cards = document.querySelector(".cards");
 
@@ -9,17 +9,17 @@ fetch(requestURL)
 	})
 	.then(function (jsonObject) {
 		console.table(jsonObject); // temporary checking for valid response and data parsin
-		const prophets = jsonObject["prophets"];
-		prophets.forEach(displayProphets);
+		const businesses = jsonObject["businesses"];
+		businesses.forEach(displayBusinesses);
 	});
 
-function displayProphets(prophet) {
+function displayBusinesses(business) {
 	// Create elements to add to the document
 	let card = document.createElement("section");
-	let h2 = document.createElement("h2");
+	let address = document.createElement("address");
 	let portrait = document.createElement("img");
-	let birthdate = document.createElement("p");
-	let birthplace = document.createElement("p");
+	let tel = document.createElement("p");
+	let ulr = document.createElement("p");
 	let order = ``;
 	switch (prophet.order) {
 		case 1:
@@ -36,23 +36,20 @@ function displayProphets(prophet) {
 	}
 
 	// Change the textContent property of the h2 element to contain the prophet's full name
-	h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-	birthdate.textContent = `Date of Birth:${prophet.birthdate} `;
-	birthplace.textContent = `Place of Birth:${prophet.birthplace}`;
+	address.textContent = `${business.address}`;
+	tel.textContent = `${business.tel} `;
+	ulr.textContent = `${business.ulr}`;
 
 	// Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
 	portrait.setAttribute("src", prophet.imageurl);
-	portrait.setAttribute(
-		"alt",
-		`Portait of ${prophet.name} ${prophet.lastname} - ${order} Latter Day President`
-	);
+	portrait.setAttribute("alt", `${business.name}`);
 	portrait.setAttribute("loading", "lazy");
 
 	// Add/append the section(card) with the h2 element
-	card.appendChild(h2);
+	card.appendChild(address);
 	card.appendChild(portrait);
-	card.appendChild(birthdate);
-	card.appendChild(birthplace);
+	card.appendChild(tel);
+	card.appendChild(ulr);
 
 	// Add/append the existing HTML div with the cards class with the section(card)
 	cards.appendChild(card);
