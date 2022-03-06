5f3524e1303c4f151da23a5ae4f18388d8e12572
ribeka <ribekananjo@gmail.com>
2022-03-06 13:41:29 +0900

    updated

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -15,40 +15,26 @@ fetch(requestURL)
 function displayCompanies(company) {
 	// Create elements to add to the document
 	let card = document.createElement("section");
-	let h2 = document.createElement("h2");
+	let add = document.createElement("p");
 	let portrait = document.createElement("img");
-	let birthdate = document.createElement("p");
-	let birthplace = document.createElement("p");
-	let order = ``;
-	switch (company.order) {
-		case 1:
-			order = `${company.order}st`;
-			break;
-		case 2:
-			order = `${company.order}nd`;
-			break;
-		case 3:
-			order = `${company.order}rd`;
-			break;
-		default:
-			order = `${company.order}th`;
-	}
+	let tel = document.createElement("p");
+	let url = document.createElement("p");
 
-	// Change the textContent property of the h2 element to contain the prophet's full name
-	h2.innerHTML = `${company.name}`;
-	birthdate.textContent = `Date of Birth:${company.birthdate} `;
-	birthplace.textContent = `Place of Birth:${company.birthplace}`;
+	// Change the textContent property of the element to contain company name
+	add.textContent = `${company.add}`;
+	tel.textContent = `${company.tel} `;
+	url.textContent = `${company.url}`;
 
-	// Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+	// Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
 	portrait.setAttribute("src", company.imageurl);
-	portrait.setAttribute("alt", `Logo of ${company.name} - ${order} image`);
+	portrait.setAttribute("alt", `Logo of ${company.name}image`);
 	portrait.setAttribute("loading", "lazy");
 
 	// Add/append the section(card) with the h2 element
-	card.appendChild(h2);
 	card.appendChild(portrait);
-	card.appendChild(birthdate);
-	card.appendChild(birthplace);
+	card.appendChild(add);
+	card.appendChild(tel);
+	card.appendChild(url);
 
 	// Add/append the existing HTML div with the cards class with the section(card)
 	cards.appendChild(card);
