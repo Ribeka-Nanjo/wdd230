8a008246cfb7da9763e300122765b1cf264c9604
ribeka <ribekananjo@gmail.com>
2022-03-11 11:24:07 +0900

    updated

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,17 @@
+const apiURL =
+	"http://api.openweathermap.org/data/2.5/weather?id=5861897&q=Fairbanks&units=imperial&appid=74a52b658ebc6441b78bea6998377dfd";
+fetch(apiURL)
+	.then((response) => response.json())
+	.then((jsObject) => {
+		console.log(jsObject);
+		document.querySelector("#current-temp").textContent =
+			jsObject.main.temp;
+
+		const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+		const desc = jsObject.weather[0].description;
+
+		document.querySelector("#icon-src").textContent = iconsrc;
+		document.querySelector("#weathericon").setAttribute("src", iconsrc);
+		document.querySelector("#weathericon").setAttribute("alt", desc);
+		document.querySelector("figcaption").textContent = desc;
+	});
