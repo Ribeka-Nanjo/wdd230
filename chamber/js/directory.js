5f2cdb8322bbb1fc1469f31336c1bce79c3a3593
ribeka <ribekananjo@gmail.com>
2022-03-20 09:54:39 +0900

    updated

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -54,3 +54,34 @@ let listView = function () {
 	document.querySelector(".cards").classList.add("listV");
 	document.querySelector(".cards").classList.remove("cardV");
 };
+
+//Footer
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+//Header
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+//Humberger Menu
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("open");
+	document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+//Banner text display
+
+const named = document.getElementById("min-banner");
+const date = new Date();
+const todaysdate = date.getDay();
+if (todaysdate == 1 || todaysdate == 2) {
+	let x = document.querySelector("#text");
+	x.innerHTML = `🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.`;
+}
--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -26,3 +26,24 @@ const imageObserver = new IntersectionObserver((items, imageObserver) => {
 images.forEach((image) => {
 	imageObserver.observe(image);
 });
+
+//Footer
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+//Header
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+//Humberger Menu
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("open");
+	document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
