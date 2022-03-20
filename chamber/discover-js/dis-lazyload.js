c29bb66cbd268cd068662f27acfc0a08b76b9a79
ribeka <ribekananjo@gmail.com>
2022-03-20 11:07:03 +0900

    updated

--- chamber/discover-js/dis-lazyload.js
+++ chamber/discover-js/dis-lazyload.js
@@ -32,7 +32,31 @@ document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
 
-//Header
+// VISIT NUMBER //
+
+// initialize display elements
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+// show todays date.
+todayDisplay.textContent = Date.now();
+
+// Header //
 const datefield = document.querySelector(".date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
@@ -40,7 +64,7 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
-//Humberger Menu
+//Humberger Menu //
 function toggleMenu() {
 	document.getElementById("primaryNav").classList.toggle("open");
 	document.getElementById("hamburgerBtn").classList.toggle("open");
